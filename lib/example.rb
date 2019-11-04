4ca654573e43722672607ac776f5b390e6994d7b
Nethelbert <gilblasse@gmail.com>
2019-11-04 12:19:26 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,5 @@
 # Use this space to code along with the readme
 def greeting do 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  phrase = "Hello World!"
+  puts phrase
+end 
